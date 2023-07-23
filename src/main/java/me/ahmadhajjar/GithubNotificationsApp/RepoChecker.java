44dfb1f01f1c3dc87b892eb5e9df07f526df0cd0
Ahmad Hajjar <ahmed_hajjar@hotmail.com>
2023-07-23 14:56:19 +0200

    TrayAdapter is not a dependency of RepoChecker anymore and other enhancements. * Don't start checking the PRs immediately on start * If there is an error the repo won't be saved to the known PRs file * Notifications happen in a separate thread. * Refactoring

--- src/main/java/me/ahmadhajjar/GithubNotificationsApp/RepoChecker.java
+++ src/main/java/me/ahmadhajjar/GithubNotificationsApp/RepoChecker.java
@@ -13,22 +13,24 @@ import java.util.Map;
 
 public class RepoChecker extends Thread {
     public static final long ONE_MINUTE = 60 * 1000;
+    private static final long INITIAL_WAIT_TIME = 5000;
     private final GitHubAPIService gitHubAPIService;
-    private final TrayAdapter trayAdapter;
     private final StorageService storageService;
     Map<String, List<Integer>> reposPRMap;
 
-    public RepoChecker(GitHubAPIService gitHubAPIService, TrayAdapter trayAdapter) {
+    public RepoChecker(GitHubAPIService gitHubAPIService) {
         this.gitHubAPIService = gitHubAPIService;
-        this.trayAdapter = trayAdapter;
         this.storageService = DiskStorageService.getInstance();
     }
 
     @Override
     public void run() {
         try {
+            Thread.sleep(INITIAL_WAIT_TIME);
+            //noinspection InfiniteLoopStatement
             while (true) {
                 checkReposForOpenPullRequests();
+                //noinspection BusyWait
                 Thread.sleep(ONE_MINUTE);
             }
         } catch (Throwable e) {
@@ -43,36 +45,57 @@ public class RepoChecker extends Thread {
         for (String repoName : repos) {
             JSONArray pullRequests = gitHubAPIService.getLatestPullRequestsForRepo(repoName);
 
-            if (!reposPRMap.containsKey(repoName)) {
-                // Store the pull request numbers
-                List<Integer> prNumbers = new ArrayList<>();
-                for (int j = 0; j < pullRequests.length(); j++) {
-                    JSONObject pr = pullRequests.getJSONObject(j);
-                    int prNumber = pr.getInt("number");
-                    prNumbers.add(prNumber);
-                }
-                reposPRMap.put(repoName, prNumbers);
-                continue; // Skip notification for the first check
+            if (pullRequests == null) {
+                continue;
             }
+
+            boolean newRepo = !reposPRMap.containsKey(repoName);
 
             List<Integer> lastPRNumbers = reposPRMap.get(repoName);
-            List<Integer> newPRNumbers = new ArrayList<>();
+            List<Integer> newPRNumbers = createPullRequestsNumbersList(pullRequests);
 
-            for (int j = 0; j < pullRequests.length(); j++) {
-                JSONObject pr = pullRequests.getJSONObject(j);
-                int prNumber = pr.getInt("number");
-                newPRNumbers.add(prNumber);
-
-                // Check if the pull request number is new
-                if (!lastPRNumbers.contains(prNumber)) {
-                    trayAdapter.sendNotification(repoName, prNumber);
-                }
-            }
-
-            // Update the last recorded pull request numbers
             reposPRMap.put(repoName, newPRNumbers);
+
+            if (!newRepo) {
+                var repoNotificationThread = new RepoNotificationThread(repoName, lastPRNumbers, newPRNumbers);
+                repoNotificationThread.start();
+            }
         }
         storageService.saveReposPRList(reposPRMap);
     }
 
+    private static List<Integer> createPullRequestsNumbersList(JSONArray pullRequests) {
+        List<Integer> prNumbers = new ArrayList<>();
+        for (int j = 0; j < pullRequests.length(); j++) {
+            JSONObject pr = pullRequests.getJSONObject(j);
+            int prNumber = pr.getInt("number");
+            prNumbers.add(prNumber);
+        }
+        return prNumbers;
+    }
+
+    private static class RepoNotificationThread extends Thread {
+
+        private final String repoName;
+        private final List<Integer> lastPRNumbers;
+        private final List<Integer> newPRNumbers;
+
+        public RepoNotificationThread(String repoName, List<Integer> lastPRNumbers, List<Integer> newPRNumbers){
+            this.repoName = repoName;
+            this.lastPRNumbers = lastPRNumbers;
+            this.newPRNumbers = newPRNumbers;
+        }
+        @Override
+        public void run() {
+            notifyAboutNewPullRequests();
+        }
+
+        private void notifyAboutNewPullRequests() {
+            for (Integer newPRNumber : newPRNumbers) {
+                if (!lastPRNumbers.contains(newPRNumber)) {
+                    TrayAdapter.getInstance().sendNotification(repoName, newPRNumber);
+                }
+            }
+        }
+    }
 }
