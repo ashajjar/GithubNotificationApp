6ac28a661a6d0bdb387cd79f76569fe0e529bf2a
Ahmad Hajjar <ahmad.hajjar@otto.de>
2023-07-28 00:02:06 +0200

    Enhanced Mac Support

--- src/main/java/me/ahmadhajjar/GithubNotificationsApp/Main.java
+++ src/main/java/me/ahmadhajjar/GithubNotificationsApp/Main.java
@@ -10,6 +10,7 @@ public class Main {
     private static final String GITHUB_TOKEN = System.getenv("GITHUB_TOKEN");
 
     public static void main(String[] args) {
+        System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS");
         logger.debug("Starting application ...");
         GitHubAPIService gitHubAPIService = new GitHubAPIService(GITHUB_TOKEN);
 
